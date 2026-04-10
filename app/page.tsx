2465ca1733fa2a3532736aaa2f58376bcbbb3d14
Qasem Mohammadi <qasimmohammadi520@gmail.com>
2026-04-10 14:52:21 +0430

    implement search and movie listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+import Loader from "@/components/Loader";
+import MovieList from "@/components/MovieList";
+import SearchBar from "@/components/SearchBar";
+import { searchMovies } from "@/lib/api";
 
 export default function Home() {
+  const [movies, setMovies] = useState<Show[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async (q: string) => {
+    if (!q) return;
+    setLoading(true);
+    const data = await searchMovies(q);
+    setMovies(data);
+    setLoading(false);
+  };
+
   return (
-    <div>hello</div>
+    <main className="p-4">
+      <SearchBar onSearch={handleSearch} />
+      {loading ? <Loader /> : <MovieList movies={movies} />}
+    </main>
   );
 }

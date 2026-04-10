fb9d23ea7769e1b16cbbe9074e759e7b21617ad7
Qasem Mohammadi <qasimmohammadi520@gmail.com>
2026-04-10 14:44:54 +0430

    add movie card component

---
+++ components/MovieCard.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { Show } from "@/types/movie";
+
+export default function MovieCard({ movie }: { movie: Show }) {
+  return (
+    <Link href={`/movie/${movie.id}`}>
+      <div className="border rounded p-2 hover:shadow">
+        {movie.image && (
+          <img src={movie.image.medium} alt={movie.name} />
+        )}
+        <h2>{movie.name}</h2>
+        <p>{movie.premiered}</p>
+      </div>
+    </Link>
+  );
+}
